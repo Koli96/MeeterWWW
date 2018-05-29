65e4f0695e02c53920209b7594c252a40e40e220
Damian Nurzyński <damiannnurzynski@gmail.com>
2018-05-29 15:42:42 +0200

    repare routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,46 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 
 import Header from "./shared/header";
 import Footer from "./shared/footer";
 import Home from "./home";
-import Login from "./login";
-import Register from "./register";
-import Account from "./protected/account";
-import Dashboard from "./protected/dashboard";
 
-function PrivateRoute({ component: Component, authed, ...rest }) {
+export function PrivateRoute({ component: Component, authed, ...rest }) {
   return (
-    <Route{...rest} render={props =>
-      authed === true ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "/login", state: { from: props.location } }}/>
-      )
-    }/>
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
   );
 }
 
-function PublicRoute({ component: Component, authed, ...rest }) {
+export function PublicRoute({ component: Component, authed, ...rest }) {
   return (
-    <Route{...rest} render={props =>
-      authed === false ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/dashboard"/>
-      )
-    }/>
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/dashboard" />
+        )
+      }
+    />
   );
 }
-
 
 export default class App extends Component {
   state = {
@@ -43,16 +51,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router onUpdate={() => window.scrollTo(0, 0)}>
-          <div>
-            <Header/> <Switch> <Route path='/' exact component={Home}/>
-            <PublicRoute authed={this.state.authed} path='/login' component={Login}/>
-            <PublicRoute authed={this.state.authed} path='/register' component={Register}/>
-            <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard}/>
-            <PrivateRoute authed={this.state.authed} path='/account' component={Account}/>
-          </Switch> <Footer/>
-          </div>
-        </Router>
+        <div>
+          <Header />
+          <Home />
+          <Footer />
+        </div>
       </div>
     );
   }
