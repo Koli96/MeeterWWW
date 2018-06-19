6b13b1ed818dc5ad19d9d470c4ffc8e38ffcf8d6
paulinaszablowska <paulinaszablowska0406@gmail.com>
2018-06-19 23:53:36 +0200

    Edycja strony głównej

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import '../styles/css/main.css'
 import heroImg from "../assets/img/interview.png";
 
 class Home extends React.Component {
@@ -8,10 +8,12 @@ class Home extends React.Component {
 			<main className="main-section">
 				<div className="container">
 					<div className="row">
-						<h1 className="col-10 mx-auto page-title">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit
+						<h1 className="headerText">
+							Meeter
 						</h1>
-						<img className="hero-img" src={heroImg} alt="Interview" />{" "}
+						<h2 className="mainText">Bliżej ludzi, bliżej Ciebie!</h2>
+						
+						<img className="hero-img animateImg" src={heroImg} alt="Interview" />{" "}
 						{/*<p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aspernatur, natus! Aspernatur cum est expedita minima placeat provident reiciendis.</p>*/}
 					</div>
 				</div>
