5c79135ecc1838ef93003e8badca50c9c98be93b
paulinaszablowska <paulinaszablowska0406@gmail.com>
2018-07-01 22:38:04 +0200

    Wypelnienie zakladkki Konto

--- src/components/protected/account.js
+++ src/components/protected/account.js
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+	Link
+} from 'react-router-dom';
+import {
+	Col,
+	Row
+} from "react-bootstrap";
+import "../../styles/css/main.css"
+import heroImg from "../../assets/img/interview.png"
 
 export default class Account extends React.Component {
 	render() {
-		return <div>hello from account</div>;
+		return (
+			<div>
+				<Row >
+					<Col sm={6}>
+						<img
+							className="hero-img animateImg"
+							src={heroImg}
+							alt="Interview"
+						/>
+					</Col>
+					<Col sm={6}>
+						< h3 className="headerText" > Witaj {this.props.userName}</h3 >
+						<h4 className="mainText">Wybierz co chcesz zrobić : </h4>
+						<Link className="nav-link accountLink" to="/events/add">
+							Dodaj wydarzenie
+										</Link>
+						<Link className="nav-link accountLink" to="/events/list">
+							Przeglądaj wydarzenia
+										</Link>
+					</Col>
+				</Row>
+			</div>
+		);
 	}
 }
